4ddd1bf0ee97cb5a614a3d68de0abb8bf7c02c3a
mannumishra <mannumishra2207@outlook.com>
2024-05-04 15:25:43 +0530

    add change

--- src/pages/CategoryPage/CategoryPage.js
+++ src/pages/CategoryPage/CategoryPage.js
@@ -51,37 +51,40 @@ const CategoryPage = () => {
             <section className="tables">
                 <div className="container">
                     <div className="row">
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">S.no</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Description</th>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Image</th>
-                                    <th scope="col">Update</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {categories && categories.reverse().map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{item.categoryname}</td>
-                                        <td>{item.description}</td>
-                                        <td><img src={item.image} alt="" style={{ height: 100 }} /></td>
-                                        <td><img src={item.image1} alt="" style={{ height: 100 }} /></td>
-                                        <td><img src={item.image2} alt="" style={{ height: 100 }} /></td>
-                                        <td><img src={item.image3} alt="" style={{ height: 100 }} /></td>
-                                        <td className='upd-btns'>
-                                            <Link to={`/edit-category/${item._id}`} className='upd-btns update'><i className="fa-solid fa-pen-to-square"></i></Link>
-                                            <button onClick={() => { hadndleDelete(item._id) }} className='upd-btns delete'><i className="fa-solid fa-trash-arrow-up"></i></button>
-                                        </td>
+                        <div className="table-responsive">
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">S.no</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Update</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {categories && categories.reverse().map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.categoryname}</td>
+                                            <td>{item.description}</td>
+                                            <td><img src={item.image} alt="" style={{ maxWidth: "100%", height: "auto" }} /></td>
+                                            <td><img src={item.image1} alt="" style={{ maxWidth: "100%", height: "auto" }} /></td>
+                                            <td><img src={item.image2} alt="" style={{ maxWidth: "100%", height: "auto" }} /></td>
+                                            <td><img src={item.image3} alt="" style={{ maxWidth: "100%", height: "auto" }} /></td>
+                                            <td className='upd-btns'>
+                                                <Link to={`/edit-category/${item._id}`} className='upd-btns update'><i className="fa-solid fa-pen-to-square"></i></Link>
+                                                <button onClick={() => { handleDelete(item._id) }} className='upd-btns delete'><i className="fa-solid fa-trash-arrow-up"></i></button>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
             </section>
--- src/pages/ContactPage/Contactpage.jsx
+++ src/pages/ContactPage/Contactpage.jsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 
 const Contactpage = () => {
-    const [data,setData] = useState([])
+    const [data, setData] = useState([])
 
-    const getApiData = async()=>{
+    const getApiData = async () => {
         try {
             let res = await axios.get("https://prestigebackend.onrender.com/api/contact")
             console.log(res);
@@ -15,9 +15,9 @@ const Contactpage = () => {
             console.log(error);
         }
     }
-    useEffect(()=>{
+    useEffect(() => {
         getApiData()
-    },[])
+    }, [])
     return (
         <>
             <ToastContainer />
@@ -37,34 +37,37 @@ const Contactpage = () => {
             <section className="tables">
                 <div className="container">
                     <div className="row">
-                        <table className="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th scope="col">S.no</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Phone</th>
-                                    <th scope="col">Address</th>
-                                    <th scope="col">Message</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.map((item, index) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{item.name}</td>
-                                        <td>{item.email}</td>
-                                        <td>{item.phone}</td>
-                                        <td>{item.address}</td>
-                                        <td>{item.message}</td>
-                                        {/* <td className='upd-btns'>
-                                            <Link to={`/edit-category/${item._id}`} className='upd-btns update'><i className="fa-solid fa-pen-to-square"></i></Link>
-                                            <button onClick={() => { hadndleDelete(item._id) }} className='upd-btns delete'><i className="fa-solid fa-trash-arrow-up"></i></button>
-                                        </td> */}
+                        <div className="table-responsive">
+                            <table className="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">S.no</th>
+                                        <th scope="col">Name</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone</th>
+                                        <th scope="col">Address</th>
+                                        <th scope="col">Message</th>
                                     </tr>
-                                ))}
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    {data.map((item, index) => (
+                                        <tr key={index}>
+                                            <td>{index + 1}</td>
+                                            <td>{item.name}</td>
+                                            <td>{item.email}</td>
+                                            <td>{item.phone}</td>
+                                            <td>{item.address}</td>
+                                            <td>{item.message}</td>
+                                            {/* <td className='upd-btns'>
+                        <Link to={`/edit-category/${item._id}`} className='upd-btns update'><i className="fa-solid fa-pen-to-square"></i></Link>
+                        <button onClick={() => { hadndleDelete(item._id) }} className='upd-btns delete'><i className="fa-solid fa-trash-arrow-up"></i></button>
+                    </td> */}
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+
                     </div>
                 </div>
             </section>
